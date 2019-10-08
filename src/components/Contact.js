92b6e25d664d5062ed3c4f2b3ba4758ca5a4a2e6
olarmarian <olar.marian7@gmail.com>
2019-10-08 17:21:05 +0300

    df?

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,18 +5,21 @@ export default class Contact extends React.Component{
         return (
             <section id="contact" className="contact" data-aos="fade-in">
                 <h1>Contact me</h1>
-                <form name = "contact" method="POST" className="contact-form" netlify>
+                <form method="POST"
+                        action="POST"
+                        className="contact-form" 
+                        netlify>
                     <div>
                         <p>Name : </p>
-                        <input type="text" name="name" placeholder="Enter name here"/>
+                        <input id="name" type="text" name="name" placeholder="Enter name here"/>
                     </div>    
                     <div>
                         <p>Email : </p>
-                        <input type="email" name="email"  placeholder="Enter email here"  size="170"/>
+                        <input id="email" type="email" name="email"  placeholder="Enter email here"  size="170"/>
                     </div>
                     <div>
                         <p>Message : </p>
-                        <textarea name="message" placeholder="Enter message here" cols="80" rows="5"></textarea>
+                        <textarea id="message" name="message" placeholder="Enter message here" cols="80" rows="5"></textarea>
                     </div>
                     <button className="send-btn" type="submit">Send</button>
                 </form>
